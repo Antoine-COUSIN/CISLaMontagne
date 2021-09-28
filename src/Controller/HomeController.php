cbcea0cac488b985bf11ab7e85008b4897c5af5c
Antoine-COUSIN <ac.acousin@free.fr>
2021-09-28 21:42:29 +0200

    Mise en place de la home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ChiefSpeechRepository;
+use App\Repository\StationStoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\StationCorePictureRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(StationCorePictureRepository $stationCorePictureRepository, ChiefSpeechRepository $chiefSpeechRepository, StationStoryRepository $stationStoryRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'station_core_pictures' => $stationCorePictureRepository->findAll(),
+            'chief_speeches' => $chiefSpeechRepository->findAll(),
+            'station_stories' => $stationStoryRepository->findAll(),
         ]);
     }
 }
