91e5ab8728afdf0a742dc8ba3f93d4549bcdc667
Antoine-COUSIN <ac.acousin@free.fr>
2021-09-29 16:41:24 +0200

    commit obligatoire

---
+++ public/js/validationStatusGoldenBook.js
@@ -0,0 +1,5 @@
+const validationStatus = document.getElementById('golden_book_validationStatus');
+
+window.addEventListener('load', () => {
+    validationStatus.parentElement.classList.add('inputDisappearance');
+})
