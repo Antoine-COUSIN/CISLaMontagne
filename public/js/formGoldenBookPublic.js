8a1caf85dae76e2afe2229bddff75d98c16e5c34
Antoine-COUSIN <ac.acousin@free.fr>
2021-09-30 14:09:04 +0200

    mise en place de toute la partie front sans responsive

---
+++ public/js/formGoldenBookPublic.js
@@ -0,0 +1,11 @@
+const titleDiv = document.getElementById('golden_book_title');
+
+window.addEventListener('load', () => {
+    titleDiv.parentElement.classList.add('goldenBookFormPublic');
+})
+
+const textDiv = document.getElementById('golden_book_text');
+
+window.addEventListener('load', () => {
+    textDiv.parentElement.classList.add('goldenBookFormPublic');
+})
--- src/Form/GoldenBookType.php
+++ src/Form/GoldenBookType.php
@@ -7,14 +7,19 @@ use Symfony\Component\Form\AbstractType;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class GoldenBookType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('text', CKEditorType::class)
+            ->add('title', TextType::class, [
+                'label' => 'Titre du commentaire'
+            ])
+            ->add('text', CKEditorType::class, [
+                'label' => 'Votre commentaire :'
+            ])
             ->add('validationStatus')
         ;
     }
