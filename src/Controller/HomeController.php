9936e657fde10f3c3bfb9beed16df61e3b58a283
Antoine-COUSIN <ac.acousin@free.fr>
2021-10-24 22:45:37 +0200

    Mise en place du contenu urgency_phone sans css + création du controller de contact

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(StationCorePictureRepository $stationCorePictureRepository, ChiefSpeechRepository $chiefSpeechRepository, StationStoryRepository $stationStoryRepository): Response
     {
